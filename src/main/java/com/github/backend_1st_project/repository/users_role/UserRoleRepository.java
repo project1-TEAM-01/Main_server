2eaf7ada01c6c2c1fe830d6493d558b44afc3adc
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2024-01-04 17:50:40 +0900

    Delete src directory

--- src/main/java/com/github/backend_1st_project/repository/users_role/UserRoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.backend_1st_project.repository.users_role;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRoleRepository extends JpaRepository<UserRole,Integer> {
-
-}
--- src/main/java/com/github/backend_1st_project/service/exceptions/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.github.backend_1st_project.service.exceptions;
-
-public class NotFoundException extends RuntimeException{
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
