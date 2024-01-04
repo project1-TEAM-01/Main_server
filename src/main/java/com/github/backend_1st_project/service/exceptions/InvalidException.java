02e5f75b8ba93233e8136ace218fd53c3079ac13
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2024-01-04 17:47:48 +0900

    Delete src directory

--- src/main/java/com/github/backend_1st_project/service/exceptions/InvalidException.java
+++
@@ -1,7 +0,0 @@
-package com.github.backend_1st_project.service.exceptions;
-
-public class InvalidException extends RuntimeException{
-    public InvalidException(String message) {
-        super(message);
-    }
-}
