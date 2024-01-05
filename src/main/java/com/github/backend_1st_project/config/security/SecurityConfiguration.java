fb1f0a15a0f409e177b31ae0feb89c2117d4604e
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2024-01-05 10:06:27 +0900

    [스프링 시큐리티 구현]
    스프링 시큐리티 및 토큰 관련 config 추가했습니다

---
+++ src/main/java/com/github/backend_1st_project/config/security/SecurityConfiguration.java
@@ -0,0 +1,47 @@
+package com.github.backend_1st_project.config.security;
+
+import com.github.backend_1st_project.web.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.headers().frameOptions().sameOrigin().and()
+                .formLogin().disable().csrf().disable()
+                .httpBasic().disable()
+                .rememberMe().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/resources/static/**","/api/signup","/api/login").permitAll()
+                .antMatchers("/api/comments","/api/comments/*","/api/posts","/api/posts/*","/api/logout").hasRole("USER")
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                .accessDeniedHandler(new CustomAccessDeniedHandler())
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+}
